61e4cb8c4497b7d0c3561a01fcbfca384c8e51e4
Mohammad <mohammadweex21@gmail.com>
2024-05-23 19:44:06 +0500

    add redux tookit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,93 +1,79 @@
 import { useState } from 'react';
-import { Link,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
 export default function SignIn() {
-  const [formData,setFormData]=useState({})
-  const [errors,setErrors]=useState(null)
-  const [loading,setLoading]=useState(false);
-  const navigate=useNavigate()
-
-
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
-
-
-    })
+    });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true)
-      const res = await fetch('/api/auth/signin',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(formData)
-        });
-       
-  
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
       const data = await res.json();
-      if(data.success===false){
-        setLoading(false);
-        setErrors(data.message)
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
         return;
-  
       }
-  
-      setLoading(false);
-      setErrors(null);
-      navigate('/')
-      
+      dispatch(signInSuccess(data));
+      navigate('/');
     } catch (error) {
-      setLoading(false);
-      setErrors(error.message)
-      
+      dispatch(signInFailure(error.message));
     }
-    
-  
-  }
- 
-
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-       
         <input
-          type="email"
+          type='email'
           placeholder='email'
           className='border p-3 rounded-lg'
           id='email'
           onChange={handleChange}
-          required
         />
         <input
-          type="password"
+          type='password'
           placeholder='password'
           className='border p-3 rounded-lg'
           id='password'
           onChange={handleChange}
-          required
         />
+
         <button
           disabled={loading}
-          type="submit"
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          {loading? 'Loading...':'Sign In'}
+          {loading ? 'Loading...' : 'Sign In'}
         </button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
-        <Link to="/sign-up">
+        <Link to={'/sign-up'}>
           <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
-      {errors && <p className='text-red-500 mt-5'>{errors}</p>}
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   );
 }
