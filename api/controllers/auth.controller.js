10beb2d619ced1ca9ba3ff8ca683c52becda56d6
Mohammad <mohammadweex21@gmail.com>
2024-05-23 15:33:11 +0500

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHnadler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+
 
 
 export const signup=async(req,res,next)=>{
@@ -15,4 +18,22 @@ export const signup=async(req,res,next)=>{
         
     }
 
+}
+
+export const signin=async (req,res,next)=>{
+    const {email,password}=req.body;
+    try {
+        const validUser=await User.findOne({email});
+        if(!validUser) return next(errorHnadler(404,'user not found'))
+        const validPassword=bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHnadler(401,'wrong credintials'));
+        const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        const {password: pass,...rest}=validUser._doc;
+        res.cookie('access_token',token,{httpOnly:true})
+        .status(200)
+        .json(rest);
+
+    } catch (error) {
+        next(error)
+    }
 }
