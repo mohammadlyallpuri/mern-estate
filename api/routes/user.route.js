7961eb87228fa51b503b18e4fe3579eeb53d74e5
Mohammad <mohammadweex21@gmail.com>
2024-05-26 15:28:55 +0500

    create user update api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js'; // Adjusted path
 
 const router = express.Router();
 
 // Define the /test route
-router.get('/test',test)
+router.get('/test', test);
+
+// Define the /update/:id route with token verification
+router.post('/update/:id', verifyToken, updateUser);
+
 // Export the router
 export default router;
