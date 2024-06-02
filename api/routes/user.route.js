28cb362415c64920b05ad64e8c8dd3cf894cc6bc
Mohammad <mohammadweex21@gmail.com>
2024-06-02 19:29:01 +0500

    create search api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,16 +1,14 @@
 import express from 'express';
-import { deleteUser, test, updateUser, getUserListings } from '../controllers/user.controller.js'; // Import getUserListings
-import { verifyToken } from '../utils/verifyUser.js'; // Adjusted path
+import { deleteUser, test, updateUser,  getUserListings, getUser} from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
 
 const router = express.Router();
 
-// Define the /test route
 router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
-// Define the /update/:id route with token verification
-router.post('/update/:id', verifyToken, updateUser);
-router.delete('/delete/:id', verifyToken, deleteUser);
-router.get('/listings/:id', verifyToken, getUserListings); // Corrected import of getUserListings
-
-// Export the router
 export default router;
